d7940e735ae3afcb5a4830da61a64ecbef06d8a0
estripling <estripling042@gmail.com>
2023-03-11 02:15:12 +0100

    feat: add dataframe validation

--- src/sparkit/validation.py
+++ src/sparkit/validation.py
@@ -1,13 +1,98 @@
 from sparkit import exception
 
 __all__ = (
+    "check_dataframe_equal",
     "check_row_count_equal",
     "check_row_equal",
     "check_schema_equal",
+    "is_dataframe_equal",
     "is_row_count_equal",
     "is_row_equal",
     "is_schema_equal",
 )
+
+
+def check_dataframe_equal(lft_df, rgt_df):
+    """Validate that the left and right data frames are equal.
+
+    Function performs:
+        - A schema check
+        - A row count check
+        - A row content check
+
+    Parameters
+    ----------
+    lft_df : pyspark.sql.DataFrame
+        Left data frame.
+    rgt_df : pyspark.sql.DataFrame
+        Right data frame.
+
+    Returns
+    -------
+    NoneType
+        A None value if data frames are equal.
+
+    Raises
+    ------
+    sparkit.exception.SchemaMismatchError
+        If schemas are not equal.
+    sparkit.exception.RowCountMismatchError
+        If row counts are not equal.
+    sparkit.exception.RowMismatchError
+        If rows are not equal.
+
+    See Also
+    --------
+    check_schema_equal : Validate schemas.
+    check_row_count_equal : Validate row counts.
+    check_row_equal : Validate rows.
+
+    Examples
+    --------
+    >>> import sparkit
+    >>> from pyspark.sql import Row, SparkSession
+    >>> spark = SparkSession.builder.getOrCreate()
+    >>> lft_df = spark.createDataFrame([Row(x=1, y=2), Row(x=3, y=4)])
+    >>> rgt_df = spark.createDataFrame([Row(x=1, y=2), Row(x=3, y=4)])
+    >>> sparkit.check_dataframe_equal(lft_df, rgt_df) is None
+    True
+
+    >>> lft_df = spark.createDataFrame([Row(x=1, y=2), Row(x=3, y=4)])
+    >>> rgt_df = spark.createDataFrame([Row(z=1, y="a", x=9), Row(z=3, y="b", x=8)])
+    >>> try:
+    ...     sparkit.check_dataframe_equal(lft_df, rgt_df)
+    ... except sparkit.exception.SchemaMismatchError as error:
+    ...     print(error)
+    ...
+    num_character_differences=15
+    struct<x:bigint,y:bigint>
+           |          |||  |||||||||||
+    struct<z:bigint,y:string,x:bigint>
+
+    >>> lft_df = spark.createDataFrame([Row(x=1, y=2)])
+    >>> rgt_df = spark.createDataFrame([Row(x=3, y=4), Row(x=5, y=6)])
+    >>> try:
+    ...     sparkit.check_dataframe_equal(lft_df, rgt_df)
+    ... except sparkit.exception.RowCountMismatchError as error:
+    ...     print(error)
+    ...
+    lft_row_count=1
+    rgt_row_count=2
+    lft_row_count - rgt_row_count=-1
+    lft_row_count / rgt_row_count=0.5
+
+    >>> lft_df = spark.createDataFrame([Row(x=1, y=2), Row(x=3, y=4), Row(x=5, y=6)])
+    >>> rgt_df = spark.createDataFrame([Row(x=3, y=4), Row(x=5, y=9), Row(x=7, y=8)])
+    >>> try:
+    ...     sparkit.check_dataframe_equal(lft_df, rgt_df)
+    ... except sparkit.exception.RowMismatchError as error:
+    ...     print(error)
+    ...
+    lft_count=2 rgt_count=2
+    """
+    check_schema_equal(lft_df, rgt_df)
+    check_row_count_equal(lft_df, rgt_df)
+    check_row_equal(lft_df, rgt_df)
 
 
 def check_row_count_equal(lft_df, rgt_df):
@@ -163,6 +248,66 @@ def check_schema_equal(lft_df, rgt_df):
 
     if lft_schema != rgt_schema:
         raise exception.SchemaMismatchError(lft_schema, rgt_schema)
+
+
+def is_dataframe_equal(lft_df, rgt_df):
+    """Evaluate if the left and right data frames are equal.
+
+    Function performs:
+        - A schema check
+        - A row count check
+        - A row content check
+
+    Parameters
+    ----------
+    lft_df : pyspark.sql.DataFrame
+        Left data frame.
+    rgt_df : pyspark.sql.DataFrame
+        Right data frame.
+
+    Returns
+    -------
+    bool
+        ``True`` if data frames are equal else ``False``.
+
+    See Also
+    --------
+    is_schema_equal : Evaluate schemas.
+    is_row_count_equal : Evaluate row counts.
+    is_row_equal : Evaluate rows.
+
+    Examples
+    --------
+    >>> import sparkit
+    >>> from pyspark.sql import Row, SparkSession
+    >>> spark = SparkSession.builder.getOrCreate()
+    >>> lft_df = spark.createDataFrame([Row(x=1, y=2), Row(x=3, y=4)])
+    >>> rgt_df = spark.createDataFrame([Row(x=1, y=2), Row(x=3, y=4)])
+    >>> sparkit.is_dataframe_equal(lft_df, rgt_df)
+    True
+
+    >>> lft_df = spark.createDataFrame([Row(x=1, y=2), Row(x=3, y=4)])
+    >>> rgt_df = spark.createDataFrame([Row(z=1, y="a", x=9), Row(z=3, y="b", x=8)])
+    >>> sparkit.is_dataframe_equal(lft_df, rgt_df)
+    False
+
+    >>> lft_df = spark.createDataFrame([Row(x=1, y=2)])
+    >>> rgt_df = spark.createDataFrame([Row(x=3, y=4), Row(x=5, y=6)])
+    >>> sparkit.is_dataframe_equal(lft_df, rgt_df)
+    False
+
+    >>> lft_df = spark.createDataFrame([Row(x=1, y=2), Row(x=3, y=4), Row(x=5, y=6)])
+    >>> rgt_df = spark.createDataFrame([Row(x=3, y=4), Row(x=5, y=9), Row(x=7, y=8)])
+    >>> sparkit.is_dataframe_equal(lft_df, rgt_df)
+    False
+    """
+    try:
+        check_schema_equal(lft_df, rgt_df)
+        check_row_count_equal(lft_df, rgt_df)
+        check_row_equal(lft_df, rgt_df)
+        return True
+    except exception.SparkitError:
+        return False
 
 
 def is_row_count_equal(lft_df, rgt_df):
